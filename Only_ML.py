9ff901d3dba6448dbcd3ad3264d497ff03cd4824
Mona Lisa <mnls4u@gmail.com>
2018-07-12 23:11:02 +0530

    Update Only_ML.py

--- Only_ML.py
+++ Only_ML.py
@@ -1,27 +1,3 @@
-"""import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-dataset = pd.read_csv('/home/pi/Documents/Sensor_readings/data_for_process.csv')
-x = dataset.iloc[:, [1]].values
-y = dataset.iloc[:, 2].values
-
-from sklearn.cross_validation import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20, random_state = 0)
-
-from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)
-
-
-
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(x_train, y_train)
-
-y_pred = classifier.predict(x_test)"""
-
 # Logistic Regression
 
 # Importing the libraries
